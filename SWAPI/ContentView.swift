3350b3455d989dde68b19d57d75943d501f1c4f3
Nilusha Wimalasena <nw.nilusha@gmail.com>
2025-09-08 11:29:58 +0100

    Initial Commit

---
+++ SWAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWAPI
+//
+//  Created by Nilusha Niwanthaka Wimalasena on 8/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWAPI/SWAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPIApp.swift
+//  SWAPI
+//
+//  Created by Nilusha Niwanthaka Wimalasena on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SWAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
